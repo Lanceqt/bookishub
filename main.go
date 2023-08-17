5850ce1a4b49c1aa7b384acbd1cba23bbf0d0c06
Lanceqt <andreas.frederiksen@proton.me>
2023-08-18 01:29:46 +0200

    I am actually a donkey at writeing these

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got / request\n")
+	io.WriteString(w, "This is my website!!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("Server closed\n")
+	} else if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
