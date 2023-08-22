f1d3bd4479841cae4b5755f2132b798107e43e3c
Lanceqt <andreas.frederiksen@proton.me>
2023-08-22 23:22:19 +0200

    .env without libary attempt

--- cmd/bookishub/main.go
+++ cmd/bookishub/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	//"fmt"
+	"os"
+	"net/http"
 	"html/template"
 	"log"
-	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
-func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+func getEnvVariable(path string) {
+	
+}
 
+func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles(("src/layout.html"))
+		tmpl, err := template.ParseFiles("../../web/templates/layout.html"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
