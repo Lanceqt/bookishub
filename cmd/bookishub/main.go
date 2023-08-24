03f1cc55860a64e664d3ae5cbede51fbec6c633d
Lanceqt <andreas.frederiksen@proton.me>
2023-08-24 21:10:49 +0200

    cleaning up file structure

--- cmd/bookishub/main.go
+++ cmd/bookishub/main.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-	//"fmt"
-	"os"
-	"net/http"
 	"html/template"
 	"log"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	//"github.com/joho/godotenv"
 )
 
-func getEnvVariable(path string) {
-	
-}
-
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("../../web/templates/layout.html"))
+		tmpl, err := template.ParseFiles("../../web/templates/layout.html")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -28,5 +22,6 @@ func main() {
 
 		tmpl.Execute(w, nil)
 	})
+	log.Println("Listening at localhost:7000")
 	http.ListenAndServe("localhost:7000", r)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/Lanceqt/bookishub
 
 go 1.21.0
 
-require github.com/joho/godotenv v1.5.1
-
 require github.com/go-chi/chi/v5 v5.0.10
